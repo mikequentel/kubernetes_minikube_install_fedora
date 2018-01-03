02fe39184d4c6a1b620cb21917e5d79b98c434fa
mikequentel <mikequentel@yahoo.com>
2018-01-02 21:10:49 -0500

    Initial add of install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# https://kubernetes.io/docs/tasks/tools/install-minikube/
+
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+mkdir -p ~/.kube
+touch ~/.kube/config
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.24.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+kubectl cluster-info
+
+# https://kubernetes.io/docs/setup/independent/install-kubeadm/
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+setenforce 0
+# yum install -y kubelet kubeadm kubectl
+yum install -y kubelet kubeadm
+systemctl enable kubelet && systemctl start kubelet
